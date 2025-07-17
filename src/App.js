c6089b3ae286f09915ea2603eab9f2f8ef1495bb
JamalBuiltIt <Jamalparker70@gmail.com>
2025-07-17 04:44:36 -0400

    aghh

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FLHSMVPage from './home';      // your form component
 import Home1 from './home1';         // page to redirect to after form submission
-import Success from './success';
 import './App.css';
 
 function App() {
